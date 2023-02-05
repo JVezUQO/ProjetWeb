2ddd33b72efd4e52d1019b22215a037ee8380c14
CA-WillMc <122403149+CA-WillMc@users.noreply.github.com>
2023-02-05 00:16:18 -0500

    JS addButton() Ajoute un courriel a la liste

--- Scripts/TopNav.js
+++ Scripts/TopNav.js
@@ -1,9 +1,14 @@
-
-
-function switchpagecarnet(){
-alert("Test1");
+function switchpagecarnet() {
+  alert("Test1");
 }
 
-function switchpageemail(){
-alert("Test2");
+function switchpageemail() {
+  alert("Test 2");
+}
+let nb_courriel = 5;
+function addButton() {
+  var buttonList = document.getElementById("button-list");
+  var newButton = document.createElement("li");
+  newButton.innerHTML = `<button class='button'>Button ${(nb_courriel += 1)} </button>`;
+  buttonList.appendChild(newButton);
 }
