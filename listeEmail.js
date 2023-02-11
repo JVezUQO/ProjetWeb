7d776c7ce6ccf6ac67e9062c60eb33517464f332
CA-WillMc <122403149+CA-WillMc@users.noreply.github.com>
2023-02-10 23:37:34 -0500

    auto fill search - temp api

--- listeEmail.js
+++ listeEmail.js
@@ -1,13 +1,46 @@
-let nb_courriel = 5;
-function addButton() {
+const userCardTemplate = document.querySelector("[data-user-template]");
+const userCardContainer = document.querySelector("[data-user-cards-container]");
+const searchInput = document.querySelector("[data-search]");
+
+let users = [];
+
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+  users.forEach((user) => {
+    const isVisible =
+      user.name.toLowerCase().includes(value) ||
+      user.email.toLowerCase().includes(value);
+    user.element.classList.toggle("hide", !isVisible);
+  });
+});
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) => res.json())
+  .then((data) => {
+    users = data.map((user) => {
+      const card = userCardTemplate.content.cloneNode(true).children[0];
+      const header = card.querySelector("[data-header]");
+      const body = card.querySelector("[data-body]");
+      header.textContent = user.name;
+      body.textContent = user.email;
+      userCardContainer.append(card);
+      return { name: user.name, email: user.email, element: card };
+    });
+  });
+
+function addEmail() {
   var buttonList = document.getElementById("button-list");
   var newButton = document.createElement("li");
-  newButton.innerHTML = ` <div class="email" onclick="addButton()">
-  <ul id="email-titre" class="email-titre">
-    <li class="liste-Titre">Titre</li>
-    <li class="liste-Contact">Contact</li>
-    <li class="liste-ligneCourriel">premiere ligne du courriel...</li>
-  </ul>
-</div>`;
+  newButton.innerHTML = `<li>
+  <div class="email-card" onclick="addButton()">
+    <div class="email">s</div>
+    <div class="titre">d</div>
+    <div class="contact">s</div>
+    <div class="email-content"></div>
+  </div>
+</li>`;
   buttonList.appendChild(newButton);
+}
+function temp() {
+  alert("Va ouvrir le courriel");
 }
