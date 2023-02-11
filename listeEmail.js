77649958953b3ef344853c505be69bbd1fe24f99
CA-WillMc <122403149+CA-WillMc@users.noreply.github.com>
2023-02-10 20:39:20 -0500

    search

--- listeEmail.js
+++ listeEmail.js
@@ -2,6 +2,12 @@ let nb_courriel = 5;
 function addButton() {
   var buttonList = document.getElementById("button-list");
   var newButton = document.createElement("li");
-  newButton.innerHTML = `<button class='button'>Button ${(nb_courriel += 1)} </button>`;
+  newButton.innerHTML = ` <div class="email" onclick="addButton()">
+  <ul id="email-titre" class="email-titre">
+    <li class="liste-Titre">Titre</li>
+    <li class="liste-Contact">Contact</li>
+    <li class="liste-ligneCourriel">premiere ligne du courriel...</li>
+  </ul>
+</div>`;
   buttonList.appendChild(newButton);
 }
