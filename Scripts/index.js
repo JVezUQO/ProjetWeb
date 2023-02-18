b1c16148278295635f38f6b1a614ecb40c09aeb5
JVezUQO <122834548+JVezUQO@users.noreply.github.com>
2023-02-18 16:55:54 -0500

    importjs

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,11 +2,20 @@ function moduleload(){
     fetch("ListeEmail.html")
     .then(response => response.text())
     .then(html => {
-        const content = html;
-        console.log(content);
-   
-        const importeur=document.getElementById("liste-email").innerHTML = content;
+        const contenthtml = html;
+        const importeur=document.getElementById("liste-email").innerHTML = contenthtml;
     })
+
+    fetch("listeEmail.js")
+    .then(response => response.text())
+    .then(js => {
+        const contentjs = document.createElement("listejs");
+        console.log(contentjs);
+        listejs.innerHTML = js;
+        document.getElementById("liste-email").appendChild(listejs);
+    })
+    
+
 
     }
 
