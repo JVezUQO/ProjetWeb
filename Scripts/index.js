a39ddc5c321b51273f163e1d44c2e328f873b2e3
JVezUQO <122834548+JVezUQO@users.noreply.github.com>
2023-02-19 11:41:32 -0500

    update

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,30 +1,27 @@
 function moduleload(){
-    fetch("ListeEmail.html")
-    .then(responsehtml => responsehtml.text())
-    .then(html => {
-        const contenthtml = html;
-        console.log(contenthtml);
-        const importeurhtml=document.getElementById("liste-email").innerHTML = contenthtml;
-    })
-
     fetch("ListeEmailCSS.css")
     .then(responsecss => responsecss.text())
     .then(css => {
         const contentcss = css;
-        console.log(contentcss);
+        //console.log(contentcss);
         const stylelisteemail = document.getElementById("liste-email").style = contentcss;
+    })
+
+    fetch("ListeEmail.html")
+    .then(responsehtml => responsehtml.text())
+    .then(html => {
+        const contenthtml = html;
+        //console.log(contenthtml);
+        const importeurhtml=document.getElementById("liste-email").innerHTML = contenthtml;
     })
     
     
     fetch("listeEmail.js")
     .then(responsejs => responsejs.text())
     .then(js => {
-        const contentjs = document.createElement("listejs");
+        const contentjs = js;
         console.log(contentjs);
-        const jslistemail = document.createElement("scripts")
-        contentjs.innerHTML = js;
-        document.body.appendChild(jslistemail);
-        eval(contentjs);
+     //   const jslistemail = document.getElementById("liste-email")
     })
     
     }
