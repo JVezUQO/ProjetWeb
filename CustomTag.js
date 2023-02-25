53a66660a26907ce9a29ab9da460b2ef222501c4
jac267 <jackpotchap@gmail.com>
2023-02-25 16:49:14 -0500

    init de window pour modifier le carnet addresse

--- CustomTag.js
+++ CustomTag.js
@@ -1,23 +1,27 @@
-
 /* custom tag CarnetDAdresse */
 class CarnetDAdresse extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = '<object type="text/html" data="carnetAdresse.html" class="carnetAdresseloader"></object>'
-        
-    }
+  connectedCallback() {
+    this.innerHTML =
+      '<object type="text/html" data="carnetAdresse.html" class="carnetAdresseloader"></object>';
+  }
 }
-  
-
-
 
 /* custom tag Reception */
 class Reception extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = '<object type="text/html" data="Reception.html" class="carnetAdresseloader"></object>'
-        
-    }
+  connectedCallback() {
+    this.innerHTML =
+      '<object type="text/html" data="Reception.html" class="carnetAdresseloader"></object>';
+  }
 }
- 
+
+/* custom tag Reception */
+class ModCarnetAdresse extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML =
+      '<object type="text/html" data="modCarnetAdresse.html" class="ajouter-modifier-carnet-addresse"></object>';
+  }
+}
 
 customElements.define("carnet-d-adresse", CarnetDAdresse);
 customElements.define("courriel-envoyer", Reception);
+customElements.define("mod-carnet-d-adresse", ModCarnetAdresse);
